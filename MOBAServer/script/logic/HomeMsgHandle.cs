c077c91641373ed91cc672678993f858fec9f879
LQY <lqy.Co@qq.com>
2025-04-14 17:49:55 +0800

    08_脚本08_调整（拆分）

---
+++ MOBAServer/script/logic/HomeMsgHandle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+public partial class MsgHandler
+{
+    //游戏大厅
+    //保存个性签名
+    public static void MsgSavePerson(ClientState c, MsgBase msgBase)
+    {
+        MsgSavePerson msg = (MsgSavePerson)msgBase;
+        Player player = c.player;
+        if (player == null) return;
+
+        /* EventHandler.OnDisconnect玩家下线时会调用DbManager.UpdatePlayerData保存c.player.data数据。*/
+        player.data.sign = msg.sign;
+        if (msg.sign.Length > 18)
+            msg.result = 1;
+        //msg.result默认为0;
+
+        player.Send(msg);
+    }
+
+    //查询个人数据
+    public static void MsgGetPerson(ClientState c, MsgBase msgBase)
+    {
+        MsgGetPerson msg = (MsgGetPerson)msgBase;
+        Player player = c.player;
+        if (player == null) return;
+
+        msg.grade = player.data.grade;
+        msg.coin = player.data.coin;
+        msg.sign = player.data.sign;
+
+        player.Send(msg);
+    }
+}
--- MOBAServer/script/logic/RoomMsgHandle.cs
+++ MOBAServer/script/logic/RoomMsgHandle.cs
@@ -2,34 +2,8 @@
 
 
 public partial class MsgHandler {
+    //房间系统
 
-    //保存个性签名
-    public static void MsgSavePerson(ClientState c, MsgBase msgBase){
-        MsgSavePerson msg = (MsgSavePerson)msgBase;
-        Player player = c.player;
-        if (player == null) return;
-
-/* EventHandler.OnDisconnect玩家下线时会调用DbManager.UpdatePlayerData保存c.player.data数据。*/
-        player.data.sign = msg.sign;    
-        if (msg.sign.Length > 18)
-			msg.result = 1;
-		//msg.result默认为0;
-
-        player.Send(msg);
-    }
-
-    //查询个人数据
-    public static void MsgGetPerson(ClientState c, MsgBase msgBase){
-        MsgGetPerson msg = (MsgGetPerson)msgBase;
-        Player player = c.player;
-        if (player == null) return;
-
-        msg.grade = player.data.grade;
-        msg.coin = player.data.coin;
-        msg.sign = player.data.sign;
-
-        player.Send(msg);
-    }
     //查询战绩
     public static void MsgGetAchieve(ClientState c, MsgBase msgBase){
 		MsgGetAchieve msg = (MsgGetAchieve)msgBase;
